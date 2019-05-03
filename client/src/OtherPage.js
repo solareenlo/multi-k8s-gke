72d9406182c37a76779afff4ccd75ef5f29e6d0c
solareenlo <solareenlo@gmail.com>
2019-05-04 05:09:35 +0900

    first commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      その他のページ
+      <Link to="/">ホームへ戻る</Link>
+    </div>
+  );
+};
